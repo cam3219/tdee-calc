a2261f66229c6a074c248f0cae2d7e46345b7a29
psychomantis <camdydesign@gmail.com>
2023-06-06 13:11:28 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TDEECalc from './TDEECalc';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <TDEECalc />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TDEECalc.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
+const originalTDEE = 0;
+
+const TDEECalc = () => {
+const [age, setAge] = useState(25);
+const [weight, setWeight] = useState(70);
+const [height, setHeight] = useState(170);
+const [activityLevel, setActivityLevel] = useState(1.2);
+const [TDEE, setTDEE] = useState(originalTDEE);
+
+//Females: (10*weight [kg]) + (6.25*height [cm]) – (5*age [years]) – 161
+//Males: (10*weight [kg]) + (6.25*height [cm]) – (5*age [years]) + 5
+
+/*-----
+Multiply by scale factor for activity level:
+Sedentary *1.2
+Lightly active *1.375
+Moderately active *1.55
+Active *1.725
+Very active *1.9
+-----*/
+
+  const calculateTDEE = () => {
+    const bmr = height * weight * age + activityLevel;
+    return bmr;
+  }
+
+  const clickHandler = () => {
+    setTDEE(calculateTDEE);
+  }
+
+  return(
+    <div>
+        <h1>TDEE CALCULATOR</h1>
+        <p>This calculator will output a rough estimation of your total daily energy expenditure or about how many calories you will burn in a day.
+           In order to theoretically lose one pound of fat in a week you would eat at your TDEE - 500 calories per day. 
+
+        </p>
+        <h2>
+        You are {age} years old
+    </h2>
+        <Slider
+    min={0}
+    max={99}
+    step={1}
+    value={age}
+    onChange={value => setAge(value)}
+    style={{width: '33%'}}/>
+    
+    
+    <h2>You weigh {weight} lbs</h2>
+    <Slider 
+    value = {weight}
+    min={0}
+    step = {1}
+    max = {500}
+    style = {{width: '33%'}}
+    onChange = {value => setWeight(value)}
+    />
+    <button className="CalculateButton"
+    onClick={clickHandler}
+    >Calculate</button>
+    <h2>{TDEE}</h2>
+    </div>
+  );
+}
+
+
+export default TDEECalc;
